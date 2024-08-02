a3d29df1149f8ee8e455c875311a56c9208004ef
Nabil <githubkv9@gmail.com>
2024-08-02 22:07:14 +0100

    Version 1.1

--- app.py
+++ app.py
@@ -32,20 +32,41 @@ def Get_Set_Hot(Get_Hot, Rand_Arr):
 		Set_Hot += Rand_Arr[elm]
 	return Set_Hot
 
-Hot_Arr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "p", "q", "w", "e", "r", "t", "y", "u", "i", "o", ";", "a", "s", "d", "f", "g", "h", "j", "k", "l", "/", "z", "x", "c", "v", "b", "n", "m", ",", ".", ")", "!", "@", "#", "$", "%", "^", "&", "*", "(", "P", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", ":", "A", "S", "D", "F", "G", "H", "J", "K", "L", "?", "Z", "X", "C", "V", "B", "N", "M", "<", ">", " "]
+Hot_Arr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "p", "q", "w", "e", "r", "t", "y", "u", "i", "o", ";", "a", "s", "d", "f", "g", "h", "j", "k", "l", "/", "z", "x", "c", "v", "b", "n", "m", ",", ".", ")", "!", "@", "#", "$", "%", "^", "&", "*", "(", "P", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", ":", "A", "S", "D", "F", "G", "H", "J", "K", "L", "?", "Z", "X", "C", "V", "B", "N", "M", "<", ">"]
+# Hot_Arr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "p", "q", "w", "e", "r", "t", "y", "u", "i", "o", ";", "a", "s", "d", "f", "g", "h", "j", "k", "l", "/", "z", "x", "c", "v", "b", "n", "m", ",", ".", ")", "!", "@", "#", "$", "%", "^", "&", "*", "(", "P", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", ":", "A", "S", "D", "F", "G", "H", "J", "K", "L", "?", "Z", "X", "C", "V", "B", "N", "M", "<", ">", " "]
+# Hot_Arr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "p", "q", "w", "e", "r", "t", "y", "u", "i", "o", ";", "a", "s", "d", "f", "g", "h", "j", "k", "l", "/", "z", "x", "c", "v", "b", "n", "m", ",", ".", ")", "!", "@", "#", "$", "%", "^", "&", "*", "(", "P", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", ":", "A", "S", "D", "F", "G", "H", "J", "K", "L", "?", "Z", "X", "C", "V", "B", "N", "M", "<", ">", " ", "[", "]", ]
+# Hot_Arr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "p", "q", "w", "e", "r", "t", "y", "u", "i", "o", ";", "a", "s", "d", "f", "g", "h", "j", "k", "l", "/", "z", "x", "c", "v", "b", "n", "m", ",", ".", ")", "!", "@", "#", "$", "%", "^", "&", "*", "(", "P", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", ":", "A", "S", "D", "F", "G", "H", "J", "K", "L", "?", "Z", "X", "C", "V", "B", "N", "M", "<", ">", " ", "[", "]", "_"]
 Hot_Key = input("Enter Your Hot_key: ")
 Hot_Stg = int(input("Enter Your Hot_Stg: "))
 Hot_Rng = int(input("Enter Your Hot_Rng: "))
 Rand_Arr = Get_Rand_Arr(Hot_Arr, Hot_Key, Hot_Stg, Hot_Rng)
 
+# Example : ===================================================
 # Set_Hot = input("Enter Your Set_Hot: ")
 # print("Your Get_Hot is: ")
 # print(Get_Get_Hot(Set_Hot, Rand_Arr))
 		
 # [38, 56, 28, 61, 45, 47, 56, 24, 61, 27, 51]
-Get_Hot = [38, 56, 28, 61, 45, 47, 56, 24, 61, 27, 51]
-print("Your Get_Hot is: ")
-print(Get_Set_Hot(Get_Hot, Rand_Arr))
+# Get_Hot = [38, 56, 28, 61, 45, 47, 56, 24, 61, 27, 51]
+# print("Your Get_Hot is: ")
+# print(Get_Set_Hot(Get_Hot, Rand_Arr))
 	
+
+
+# Set_Hot = input("Enter Your Set_Hot: ")
+# print("Your Get_Hot is: ")
+# print(Get_Get_Hot(Set_Hot, Rand_Arr))
+
+# Get_Hot = 
+# print("Your Get_Hot is: ")
+# print(Get_Set_Hot(Get_Hot, Rand_Arr))
+
+
+
+
+
+
+
+
 
 
